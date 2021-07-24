2c9b90dba1dbebc21c9ed133e1fd0656a1542d91
zhaoedf <610318832@qq.com>
2021-07-24 10:16:48 +0800

    new demo

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ export MLFLOW_EXPERIMENT_NAME='test2'
 . activate IL
 # Mlflow project path has to be absolute path.
 # --storage-dir http://localhost:10500
-mlflow run --no-conda -P data_file=/data/defeng/mlruns -P regularization=5.0 -e main /data/defeng/project/
+#mlflow run --no-conda -P data_file=/data/defeng/mlruns -P regularization=5.0 -e main /data/defeng/project/
+mlflow run --no-conda -P data_file=/data/defeng/mlruns -P regularization=5.0 -e main git@github.com:zhaoedf/mlflow_projects_demo.git  
--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 import argparse
+import mlflow
+import numpy as np
+
 
 def get_args():
 	parser = argparse.ArgumentParser(description='Train the UNet on images and target masks',
@@ -14,3 +17,9 @@ if __name__ == '__main__':
 	args = get_args() 
 	print(args.epochs)
 	print(args.batchsize)
+	client = MlflowClient(tracking_uri='http://localhost:10500')
+
+	data = np.random.random(100)
+
+	for idx,item in enumerate(data):
+	    client.log_metric(run.info.run_id,'data',item,step=idx)
